ba4d839d2284a47dc35dbc47700ae19e384c09b0
AlessioBonadonna <alessiobonadonna23@gmail.com>
2022-10-18 00:34:00 +0200

    add comment

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// Bonus
+// Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+// - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+// - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+// - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
 // Prendo il bottone play
 const playBUTTON = document.getElementById('play');
 
+//faccio partire la funzione play con tutto il codice dentro 
 function play() {
     let cucu = document.getElementById('cucu');
     cucu.classList.remove('noncliccabile');
@@ -12,6 +24,8 @@ function play() {
     fieldGame.innerHTML = '';
     const levelHTML = document.getElementById('livello');
     const level = levelHTML.value;
+    
+    //switch per la difficolta del gioco 
     switch (level) {
         case '1':
         default:
@@ -24,8 +38,6 @@ function play() {
             numCell = 49;
             break;
     }
-
-
     // funzione che genera la cella
     function drawCell(num) {
         const cellPerSide = Math.sqrt(numCell);
@@ -33,12 +45,9 @@ function play() {
         cell.className = 'square';
         cell.style.width = `calc(100% / ${cellPerSide})`;
         cell.style.height = `calc(100% / ${cellPerSide})`;
-        cell.innerHTML = `
-            <span></span>
-            
-        `;
-        
-            
+        cell.innerHTML = `<span></span>`;
+
+        //ciclo if con addEvent click con la funzione che mi differenza il rosso e il blu    
 
         if (bombsPosition.includes(num)) {
             cell.classList.add('bomb');
@@ -49,14 +58,11 @@ function play() {
                 }
                 cucu.classList.add('noncliccabile');
             
-            
+                /*qua va inserito la frazione di codice per far spuntare il contatore di click !!!!
 
             
-
-            } 
-            
-            
-            );
+                */
+            });
         } else {
             cell.addEventListener('click', function () {
                 this.classList.add('green');
@@ -64,8 +70,6 @@ function play() {
         }
         return cell;
     }
-
-
 
     while (bombsPosition.length < NUM_BOMB) {
         const bomb = randomNumber(1, numCell);
@@ -88,5 +92,5 @@ function play() {
     // chiamo la funzione
     drawGrid();
 }
-// attacco event listener al bottono play
+// attacco event listener al bottone play per far partire il gioco 
 playBUTTON.addEventListener('click', play);
